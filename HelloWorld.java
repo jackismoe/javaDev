2e4ba85d157259dca1c36de3d8902c38f1fb3d43
Jack <jacksonmorgan@hey.com>
2021-03-16 20:49:37 -0400

    add todays

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+//  The Java source file may only contain one public class, but it can contain multiple classes with other than public access and any number of public inner classes.
+// When the source file contains multiple classes, make one class‘public’ and name the source file with that public class name.
+// In Java, a class name and the file (in which it is written) must be the same.
